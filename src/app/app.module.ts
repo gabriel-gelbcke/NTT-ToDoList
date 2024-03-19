3cb73093a84df09bb36d8b12b9120abe6f6a8e54
Gabriel <gabriel.gelbcke16@gmail.com>
2024-03-19 13:27:18 -0300

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodolistComponent
+    TodolistComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     FormsModule,
